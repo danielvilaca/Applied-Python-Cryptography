4c0ed1fc332fc9cc7f8f52c0ade380d40cf38434
DanielVilaca <a16939@alunos.ipca.pt>
2025-03-15 22:58:38 +0000

    Data Integrity/Checksums

---
+++ Hashing/data_integrity.py
@@ -0,0 +1,31 @@
+import hashlib
+
+def calculate_checksum(file_path):
+
+    with open(file_path, "rb") as f:
+
+        file_hash = hashlib.sha256()
+
+        while chunk := f.read(4096):
+
+            file_hash.update(chunk)
+
+    return file_hash.hexdigest()
+
+file_path = "example_file.txt"
+
+checksum = calculate_checksum(file_path)
+
+print ("Checksum of ", file_path, " is: ", checksum)
+
+with open(file_path, "w") as f:
+
+    f.write("This file has been modified!")
+
+new_checksum = calculate_checksum(file_path)
+
+print ("The new Checksum is ", new_checksum)
+
+if checksum == new_checksum:
+
+    print ("This file has not been tampered with!")
